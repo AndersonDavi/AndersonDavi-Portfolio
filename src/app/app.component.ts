225e3d67dc0ed90c3cd046d8fc0e173176581701
Anderson Rueda <andesondavid17@gmail.com>
2024-05-19 22:06:16 -0500

    feat: fonts & scroll config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ThreeComponent } from './three/three.component';
+import { NavbarComponent } from './landing/components/navbar/navbar.component';
+import { SkillsComponent } from "./landing/skills/skills.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [ThreeComponent],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [ThreeComponent, NavbarComponent, SkillsComponent]
 })
 export class AppComponent {
   title = 'AndersonDavi';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{html,ts}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffbe41",
+        dark: "#2f2626",
+        grey: "#b9b9b9",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
