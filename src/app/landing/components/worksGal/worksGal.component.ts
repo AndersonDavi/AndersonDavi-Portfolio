42d56a0f40472b83799fff91172a9dcf19ad456f
AndersonDavi <andesondavid17@gmail.com>
2024-05-21 15:50:04 -0500

    feat: galeria trabajos responsive & ajustes build con assets

--- src/app/landing/components/worksGal/worksGal.component.ts
+++ src/app/landing/components/worksGal/worksGal.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ImagePipePipe } from './pipes/imagePipe.pipe';
 interface workItem {
+  id: number;
   title: string;
   description: string;
   image: string;
@@ -21,6 +22,7 @@ export class WorksGalComponent implements OnInit {
 
   public worksList: workItem[] = [
     {
+      id: 1,
       title: 'Chromatic',
       description:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quos. Ipsa impedit et dignissimos aliquam nemo cum, quisquam, fuga doloribus deserunt blanditiis soluta provident possimus maxime exercitationem eaque. Nostrum architecto explicabo illo cum, accusamus atque, optio recusandae ab dolore magni aliquid, dignissimos nam repellat excepturi. Doloribus deserunt odit minima reiciendis animi nihil, qui consequatur totam, sequi repudiandae at deleniti tenetur',
@@ -30,6 +32,7 @@ export class WorksGalComponent implements OnInit {
       sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
     },
     {
+      id: 2,
       title: 'Chromatic2',
       description: 'A color palette generator',
       image: 'assets/gal-img/madmax.png',
@@ -38,11 +41,20 @@ export class WorksGalComponent implements OnInit {
       sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
     },
     {
+      id: 3,
       title: 'Chromatic3',
       description: 'A color palette generator',
       image: 'assets/gal-img/madmax.png',
       logo: 'assets/logos/madmax.svg',
-
+      demoLink: 'https://chromatic-9f8f9.web.app/',
+      sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
+    },
+    {
+      id: 4,
+      title: 'Chromatic3',
+      description: 'A color palette generator',
+      image: 'assets/gal-img/madmax.png',
+      logo: 'assets/logos/madmax.svg',
       demoLink: 'https://chromatic-9f8f9.web.app/',
       sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
     },
