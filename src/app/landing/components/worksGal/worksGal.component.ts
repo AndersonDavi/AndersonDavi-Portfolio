e221acd12ccab836c6b3b87c7db29fa2ef9aaf86
AndersonDavi <andesondavid17@gmail.com>
2024-05-22 17:20:18 -0500

    feat: Cambios contenido galeria trabajos

--- src/app/landing/components/worksGal/worksGal.component.ts
+++ src/app/landing/components/worksGal/worksGal.component.ts
@@ -23,38 +23,12 @@ export class WorksGalComponent implements OnInit {
   public worksList: workItem[] = [
     {
       id: 1,
-      title: 'Chromatic',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quos. Ipsa impedit et dignissimos aliquam nemo cum, quisquam, fuga doloribus deserunt blanditiis soluta provident possimus maxime exercitationem eaque. Nostrum architecto explicabo illo cum, accusamus atque, optio recusandae ab dolore magni aliquid, dignissimos nam repellat excepturi. Doloribus deserunt odit minima reiciendis animi nihil, qui consequatur totam, sequi repudiandae at deleniti tenetur',
-      image: 'assets/gal-img/madmax.png',
-      logo: 'assets/logos/madmax.svg',
-      demoLink: 'https://chromatic-9f8f9.web.app/',
-      sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
-    },
-    {
-      id: 2,
-      title: 'Chromatic2',
-      description: 'A color palette generator',
-      image: 'assets/gal-img/madmax.png',
-      logo: 'assets/logos/madmax.svg',
-      demoLink: 'https://chromatic-9f8f9.web.app/',
-    },
-    {
-      id: 3,
-      title: 'Chromatic3',
-      description: 'A color palette generator',
-      image: 'assets/gal-img/madmax.png',
-      logo: 'assets/logos/madmax.svg',
-      demoLink: 'https://chromatic-9f8f9.web.app/',
-      sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
-    },
-    {
-      id: 4,
-      title: 'Chromatic3',
-      description: 'A color palette generator',
-      image: 'assets/gal-img/madmax.png',
-      logo: 'assets/logos/madmax.svg',
-      sourceLink: 'https://github.com/alexandru-georgescu/chromatic',
+      title: 'Gifs-App',
+      description: 'Proyecto personal como práctica de Angular, buscador de Gifs con historial y conexión a la API de Giphy',
+      image: 'assets/gal-img/gifsAppIMG.png',
+      logo: 'assets/logos/gifsAppLogo.png',
+      demoLink: 'https://andersondavi.github.io/GIFS-API/',
+      sourceLink: 'https://github.com/AndersonDavi/GIFS-API',
     },
   ];
   ngOnInit(): void {
